f3fb8823e4918f0dfd10ff3d82fe1b85532fdd99
Tejashri darade <132901534+daradeteju18@users.noreply.github.com>
2025-05-02 17:14:21 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -75,7 +75,8 @@ function Login() {
                                         Login
                                     </button>
                                     <p className="text-sm font-semibold mt-2 pt-1 mb-0">
-                                        Don't have an account?
+                                       
+                                        Account Nahi Hai ?
                                         <Link
                                             to={"/register"}
                                             className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out ml-5"
